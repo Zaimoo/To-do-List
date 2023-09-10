5b20e6c45c09eb33b1e81d0eaa2f4c3206c0af7b
Zaimoo <reycezartigley@gmail.com>
2023-09-11 00:22:29 +0800

    added new menu bar that has settings for auto launch on startup, and an about info that redirects to this repository.

--- public/electron.js
+++ public/electron.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, shell} = require('electron');
 const path = require("path");
 const url = require("url");
 
@@ -9,14 +9,64 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: true, // Enable Node.js integration
     },
+    icon: path.join(__dirname, "icon.ico")
   });
 
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-      }));
+  const appURL = app.isPackaged
+    ? url.format({
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file:",
+        slashes: true,
+      })
+    : "http://localhost:3000";
+  win.loadURL(appURL);
+ 
+  // Automatically open Chrome's DevTools in development mode.
+  if (!app.isPackaged) {
+    win.webContents.openDevTools();
+  }
     
+      let autoRunOnStart = false; // Initialize autoRunOnStart as false
+      const loginItemSettings = app.getLoginItemSettings();
+      autoRunOnStart = loginItemSettings.openAtLogin;
+    
+      // Create a custom menu
+      const menuTemplate = [
+        {
+          label: 'Settings',
+          submenu: [
+            {
+              label: 'Auto Run on Start',
+              type: 'checkbox',
+              checked: autoRunOnStart,
+              click: async (menuItem) => {
+                // Toggle the "Auto Run on Start" setting
+                autoRunOnStart = !autoRunOnStart;
+    
+                // Update the app's login settings
+                app.setLoginItemSettings({
+                  openAtLogin: autoRunOnStart,
+                });
+    
+                // Update the checkbox state
+                menuItem.checked = autoRunOnStart;
+              },
+            },
+          ],
+        },
+        {
+          label: 'About',
+          click: () => {
+            // Handle the "About" menu item here (open a dialog or show information)
+            // For example, you can use the shell module to open an external URL or show a dialog
+            shell.openExternal('https://github.com/Zaimoo/to-do-list');
+          },
+        },
+      ];
+    
+      const menu = Menu.buildFromTemplate(menuTemplate);
+      Menu.setApplicationMenu(menu);
+
 }
 
 app.whenReady().then(createWindow);
