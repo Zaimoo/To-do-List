d780997026c9c2e05d69545cbb556ab8b5a6beb6
Zaimoo <reycezartigley@gmail.com>
2023-09-12 02:30:38 +0800

    improved Options modal(1.1)

--- src/components/Options.js
+++ src/components/Options.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Modal, Button } from "react-bootstrap";
 import { FaSun, FaMoon } from "react-icons/fa";
 import { FaBell } from "react-icons/fa";
+import { BsFillAirplaneFill } from "react-icons/bs";
 
 function Options({
   showModal,
@@ -10,7 +11,14 @@ function Options({
   toggleDarkMode,
   notificationEnabled,
   toggleNotification,
-}) {
+  autoStart,
+  toggleAutoStart,
+  about
+}
+
+) 
+
+{
   return (
     <Modal show={showModal} onHide={closeModal}>
       <Modal.Header closeButton>
@@ -26,7 +34,7 @@ function Options({
             onChange={toggleDarkMode}
           />
           <label className="form-check-label" htmlFor="darkModeSwitch">
-            {darkMode ? <FaMoon /> : <FaSun />} Toggle Dark Mode
+            {darkMode ? <FaMoon /> : <FaSun />} Dark Mode
           </label>
         </div>
 
@@ -38,17 +46,30 @@ function Options({
             checked={notificationEnabled}
             onChange={toggleNotification}
           />
-          <label className="form-check-label mb-3" htmlFor="notificationToggle">
+          <label className="form-check-label" htmlFor="notificationToggle">
             <FaBell
               className={`bell-icon ${notificationEnabled ? "gold" : ""}`}
-            />
-            {notificationEnabled
-              ? " Disable Notifications"
-              : " Enable Notifications"}
+            /> Notifications
           </label>
         </div>
+
+        <div className="form-check form-switch mt-1">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          id="autoStartToggle"
+          checked={autoStart}
+          onChange={toggleAutoStart}
+        />
+        <label className="form-check-label mb-3" htmlFor="autoStartToggle">
+          <BsFillAirplaneFill /> Launch on Startup
+        </label>
+      </div>
       </Modal.Body>
       <Modal.Footer>
+        <Button variant="success" onClick={about}>
+          About
+        </Button>
         <Button variant="secondary" onClick={closeModal}>
           Close
         </Button>
